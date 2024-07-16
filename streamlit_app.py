e1c186d5b892eb5e4166a38702e9ee75f945925c
kanad27 <kanad27@gmail.com>
2024-07-16 11:53:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,20 @@ df = conn.query("SELECT * from mytable;", ttl=600)
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.NAME} has a :{row.PET}:")
+
+# import streamlit as st
+
+# # Initialize connection.
+# conn = st.connection("snowflake")
+
+# Load the table as a dataframe using the Snowpark Session.
+@st.cache_data
+def load_table():
+    session = conn.session()
+    return session.table("mytable").to_pandas()
+
+df = load_table()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.NAME} has a :{row.PET}:")
